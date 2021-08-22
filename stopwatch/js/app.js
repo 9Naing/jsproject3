556c8c6acecd58744ae102773bcb0cb3fe9c18e2
Swam Min Naing <swamminnaing21.smn@gmail.com>
2021-08-22 09:29:40 +0630

    22/August/

---
+++ stopwatch/js/app.js
@@ -0,0 +1,68 @@
+const display = document.querySelector('.display');
+const startbtn = document.querySelector('.start');
+const pausebtn = document.querySelector('.pause');
+const resetbtn = document.querySelector('.reset');
+
+// console.log(display, startbtn, pausebtn, resetbtn);
+// let milliseconds = 0;
+//      seconds = 0;
+//      minutes = 0;
+//      hours =  0;
+
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+
+//Event Listener
+startbtn.addEventListener('click', starttimer);
+pausebtn.addEventListener('click', pausetimer);
+resetbtn.addEventListener('click', resettimer);
+
+let time;
+//Start Timer
+function starttimer(){
+     if(time !== null){
+          clearInterval(time);
+     }
+     time =      setInterval(displaytimer, 50);
+}
+
+//Pause Timer
+function pausetimer(){
+     clearInterval(time);
+}
+
+//Reset Timer
+function resettimer(){
+     clearInterval(time);
+     [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+     display.textContent = '00 : 00 : 00';
+}
+
+//Display Timer
+function displaytimer(){
+     milliseconds += 10;
+     if(milliseconds === 1000){
+          milliseconds = 0;
+
+          seconds++;
+
+          if(seconds === 60){
+               seconds = 0;
+
+               minutes++;
+
+               if(minutes === 60){
+                    minutes = 0;
+
+                    hours++;
+               }
+          }
+     }
+
+     let h = hours < 10 ? "0" + hours : hours;
+     let m = minutes < 10? "0" + minutes : minutes;
+     let s = seconds < 10? '0' + seconds : seconds;
+     let ms = milliseconds < 10? '00' + milliseconds : milliseconds < 100 ? '0' + milliseconds : milliseconds;
+     // console.log(milliseconds);
+
+     display.innerText = `${h} : ${m} : ${s} : ${ms}`;
+}
