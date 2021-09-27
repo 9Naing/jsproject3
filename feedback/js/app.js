6e8fae7664d882a91345e6e40817ff132891a9a5
Swam Min Naing <swamminnaing21.smn@gmail.com>
2021-09-27 20:29:15 +0630

    27 Sep

---
+++ feedback/js/app.js
@@ -0,0 +1,41 @@
+//UI
+const panel = document.querySelector('#panel');
+const ratingcontainer = document.querySelector('.ratings-container');
+const ratings = document.querySelectorAll('.rating');
+const sendbtn = document.querySelector('#send');
+
+let selectedrating = "Satisfied";
+
+panel.addEventListener('click', (e) => {
+     // if(e.target.classList.contains('rating')){
+     //      console.log(e.target);
+     // }
+
+     if(e.target.parentNode.classList.contains('rating')){
+          removeactive();
+
+          e.target.parentNode.classList.add('active');
+
+          // selectedrating = e.target.nextElementSibling.textContent;
+          selectedrating = e.target.parentNode.lastElementChild.textContent;
+          // console.log(selectedrating);
+     }else if(e.target.classList.contains('rating')){
+          removeactive();
+          e.target.classList.add('active');
+     }
+});
+
+function removeactive(){
+     for(let i = 0; i < ratings.length; i++){
+          ratings[i].classList.remove('active');
+     }
+}
+
+sendbtn.addEventListener('click', () => {
+     panel.innerHTML = `
+          <i class = "fas fa-heart fa-2x"></i>
+          <strong>Thank You !</strong>
+          <strong>Feedback : ${selectedrating}</strong>
+          <p>We'll use your feedback to improve our customer support.</p>
+     `;
+});
